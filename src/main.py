8a40209ee46ea14e1a25460853a87a0a8988e9e3
b-blackwell <grasslookssuspicious@gmail.com>
2025-05-15 00:39:35 -0400

    add generate pages recursive

--- src/main.py
+++ src/main.py
@@ -8,7 +8,9 @@ def main():
 
     print("Copying static files to public directory...")
     copy_directory_contents("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+
+    print("Generating content...")
+    generate_pages_recursive("content", "template.html", "public")
 
 if __name__ == "__main__":
     main()
