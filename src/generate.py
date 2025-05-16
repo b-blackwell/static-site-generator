9fd312a34c54f3407c01a2efec0a6be39596f777
b-blackwell <grasslookssuspicious@gmail.com>
2025-05-15 23:53:40 -0400

    fix build

--- src/generate.py
+++ src/generate.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
             return header
     raise ValueError("no header found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}...")
 
     with open(from_path, 'r') as file:
@@ -23,7 +23,7 @@ def generate_page(from_path, template_path, dest_path):
 
     title = extract_title(markdown)
 
-    template = template_file.replace("{{ Title }}", title).replace("{{ Content }}", html_string).replace('href="/', 'href="{basepath}').replace('src="/', 'src="{basepath}')
+    template = template_file.replace("{{ Title }}", title).replace("{{ Content }}", html_string).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     dest_dir = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir):
@@ -32,12 +32,12 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, 'w') as file:
         file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def main():
     copy_directory_contents("static", "public")
 
     print("Generating content...")
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "public", basepath)
 
 if __name__ == "__main__":
     main()
